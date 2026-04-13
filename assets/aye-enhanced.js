413de42c02b3a23d02f77fa469cf9820eb1d0c5b
basevich <basevich@proton.me>
2026-04-13 21:37:42 +0400

    update aye-enhanced.js

--- assets/aye-enhanced.js
+++ assets/aye-enhanced.js
@@ -29,3 +29,20 @@ $("#showplaylistmanager").remove();
 $("#qlockbtn").remove();
 
 $.getScript("https://kinach.vercel.app/assets/Crypto.js");
+
+// Localize mod flair button
+$('#modflair').text('Мод');
+
+// Add custom logo
+$("img[src$='header-logo-short.png']")
+    .attr("id","nav-logo")
+    .attr("src","https://i.imgur.com/6guda2s.png")
+    .attr("style","width:80px");
+
+// Random byte
+function randomByte() {
+  return 'xxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
