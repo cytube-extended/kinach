7837e29bb8bb99d4d6160a1fa9e40bf1bc8cd26e
basevich <basevich@proton.me>
2026-04-14 03:37:19 +0400

    add and enable module 22

--- assets/GlobalModules.js
+++ assets/GlobalModules.js
@@ -61,7 +61,7 @@
       __webpack_require__(17);
       __webpack_require__(18);
       __webpack_require__(19);
-      ////__webpack_require__(22);
+      __webpack_require__(22);
       ////__webpack_require__(27);
       ////__webpack_require__(28);
       ////__webpack_require__(32);
@@ -21483,6 +21483,379 @@
           if (settings.isDirty(namespace + '.enabled')) {
             app.Settings.requestPageReload();
           }
+        });
+      });
+
+      /***/
+    },
+    /* 22 */
+    /***/ function (module, exports) {
+      window.cytubeEnhanced.addModule('favouritePictures', function (app) {
+        'use strict';
+        var that = this;
+
+        // Set defaults
+        var favouritePicturesFromV1 = app.parseJSON(
+          window.localStorage.getItem('favouritePictures'),
+          [],
+        );
+        app.storage.setDefault(
+          'favouritePictures',
+          _.isArray(favouritePicturesFromV1) ? favouritePicturesFromV1 : [],
+        );
+
+        // Show / Hie fav pics
+        this.$toggleFavouritePicturesPanelBtn = $(
+          '<span id="favourite-pictures-btn" title="' +
+            app.t('favPics[.]Show your favorite images') +
+            '" style="padding: 5px 0px;cursor: pointer">',
+        ).html(
+          '<img src="https://i.imgur.com/w0wVICg.png" style="max-height: 40px;" />',
+        );
+
+        // Init
+        if ($('#favourite-pictures-btn').length == 0) {
+          this.$toggleFavouritePicturesPanelBtn.insertAfter('#emotelistbtn');
+
+          // Row above emotes
+          this.$favpicsrow = $('<div class="row" id="favpicsrow">').insertAfter(
+            '#controlsrow',
+          );
+
+          // Panel inside the row
+          this.$favouritePicturesPanel = $(
+            '<div id="favourite-pictures-panel">',
+          )
+            .prependTo(this.$favpicsrow)
+            .hide();
+          this.$favouritePicturesPanelRow = $(
+            '<div class="favourite-pictures-panel-row">',
+          ).appendTo(this.$favouritePicturesPanel);
+
+          this.$favouritePicturesTrash = $(
+            '<div id="pictures-trash" title="' +
+              app.t('favPics[.]Drop the picture here to remove it') +
+              '">',
+          )
+            .append('<i class="pictures-trash-icon glyphicon glyphicon-trash">')
+            .appendTo(this.$favouritePicturesPanelRow);
+
+          this.$favouritePicturesBodyPanel = $(
+            '<div id="pictures-body-panel">',
+          ).appendTo(this.$favouritePicturesPanelRow);
+
+          this.$favouritePicturesControlPanel = $(
+            '<div id="pictures-control-panel" class="row">',
+          ).appendTo(this.$favouritePicturesPanel);
+
+          this.$favouritePicturesControlPanelForm = $('<div class="col-md-12">')
+            .html(
+              '<div class="input-group">' +
+                '<span class="input-group-btn">' +
+                '<button id="help-pictures-btn" class="btn btn-sm btn-default" style="border-radius:0;" type="button">?</button>' +
+                '</span>' +
+                '<span class="input-group-btn">' +
+                '<button id="export-pictures" class="btn btn-sm btn-default" style="border-radius:0;" type="button">' +
+                app.t('favPics[.]Export pictures') +
+                '</button>' +
+                '</span>' +
+                '<span class="input-group-btn">' +
+                '<label for="import-pictures" class="btn btn-sm btn-default" style="border-radius:0;">' +
+                app.t('favPics[.]Import pictures') +
+                '</label>' +
+                '<input type="file" style="display:none;" id="import-pictures" name="pictures-import">' +
+                '</span>' +
+                '<input type="text" id="picture-address" class="form-control input-sm" placeholder="' +
+                app.t('favPics[.]Picture url') +
+                '">' +
+                '<span class="input-group-btn">' +
+                '<button id="add-picture-btn" class="btn btn-sm btn-default" style="border-radius:0;" type="button">' +
+                app.t('favPics[.]Add') +
+                '</button>' +
+                '</span>' +
+                '</div>',
+            )
+            .appendTo(this.$favouritePicturesControlPanel);
+        }
+
+        // Ignore
+        this.makeSmilesAndPicturesTogether = function () {
+          that.smilesAndPicturesTogether = true;
+          that.$toggleFavouritePicturesPanelBtn.hide();
+          that.$favouritePicturesPanel.hide();
+        };
+
+        this.entityMap = {
+          '&': '&amp;',
+          '<': '&lt;',
+          '>': '&gt;',
+          '"': '&quot;',
+          "'": '&#39;',
+        };
+        this.replaceUnsafeSymbol = function (symbol) {
+          return that.entityMap[symbol];
+        };
+
+        // Show fav pics (refresh)
+        this.renderFavouritePictures = function () {
+          // Get pics from storage
+          var favouritePictures = app.storage.get('favouritePictures') || [];
+
+          // Clear current pics
+          this.$favouritePicturesBodyPanel.empty();
+
+          // Fill in
+          for (
+            var n = 0, favouritePicturesLen = favouritePictures.length;
+            n < favouritePicturesLen;
+            n++
+          ) {
+            var escapedAddress = favouritePictures[n].replace(
+              /[&<>"']/g,
+              this.replaceUnsafeSymbol,
+            );
+
+            $('<img class="favourite-picture-on-panel">')
+              .attr({ src: escapedAddress })
+              .appendTo(this.$favouritePicturesBodyPanel)
+              .on('click', e =>
+                that.insertFavouritePicture($(e.target).attr('src')),
+              )
+              .bind('contextmenu', () => {
+                return false;
+              })
+              .on('contextmenu', e =>
+                app.Helpers.addMessageToChatInput(
+                  ' [f] ' + $(e.target).attr('src') + ' [/f] ',
+                  'end',
+                ),
+              );
+          }
+        };
+
+        // Add to chatline input
+        this.insertFavouritePicture = function (address) {
+          app.Helpers.addMessageToChatInput(' ' + address + ' ', 'end');
+        };
+
+        // Button click
+        this.handleFavouritePicturesPanel = function (
+          $toggleFavouritePicturesPanelBtn,
+        ) {
+          var smilesAndPicturesTogether =
+            this.smilesAndPicturesTogether || false;
+
+          if ($('#smiles-panel').length !== 0 && !smilesAndPicturesTogether) {
+            $('#smiles-panel').hide();
+          }
+
+          this.$favouritePicturesPanel.slideToggle(200);
+
+          if (!smilesAndPicturesTogether) {
+            if ($toggleFavouritePicturesPanelBtn.hasClass('favImg-default')) {
+              if (
+                $('#smiles-btn').length !== 0 &&
+                $('#smiles-btn').hasClass('btn-success')
+              ) {
+                $('#smiles-btn').removeClass('btn-success');
+                $('#smiles-btn').addClass('btn-default');
+              }
+
+              $toggleFavouritePicturesPanelBtn.removeClass('favImg-default');
+              $toggleFavouritePicturesPanelBtn.addClass('favImg-success');
+            } else {
+              $toggleFavouritePicturesPanelBtn.removeClass('favImg-success');
+              $toggleFavouritePicturesPanelBtn.addClass('favImg-default');
+            }
+          }
+        };
+        this.$toggleFavouritePicturesPanelBtn.on('click', function () {
+          // Load images on first click
+          if ($('#pictures-body-panel').children().length === 0) {
+            that.renderFavouritePictures();
+          }
+          that.handleFavouritePicturesPanel($(this));
+        });
+
+        this.addFavouritePicture = function (imageUrl) {
+          imageUrl = _.trim(imageUrl);
+          if (imageUrl !== '') {
+            var favouritePictures = app.storage.get('favouritePictures') || [];
+
+            if (favouritePictures.indexOf(imageUrl) === -1) {
+              // Add image if it is not present in the array
+              favouritePictures.push(imageUrl);
+            } else {
+              window
+                .makeAlert(app.t('favPics[.]The image already exists'))
+                .prependTo(this.$favouritePicturesBodyPanel);
+              $('#picture-address').val('');
+
+              return false;
+            }
+
+            // Clear address input
+            $('#picture-address').val('');
+
+            // Set updated array
+            app.storage.set('favouritePictures', favouritePictures);
+
+            // Reload the list if images
+            this.renderFavouritePictures();
+          }
+        };
+        $('#add-picture-btn').on('click', function (e) {
+          e.preventDefault();
+
+          that.addFavouritePicture($('#picture-address').val().trim());
+        });
+        $('#picture-address').on('keypress', function (e) {
+          if (e.which == 13) {
+            that.addFavouritePicture($('#picture-address').val().trim());
+          }
+        });
+
+        this.showHelp = function () {
+          var $header = $('<div class="modal-header__inner">');
+          $header.append($('<h3 class="modal-title">').text(app.t('Help')));
+
+          var $wrapper = $('<div class="help-pictures-content">');
+          $wrapper.append(
+            $(
+              '<p>' +
+                app.t(
+                  'favPics[.]<p>Favourite pictures feature if for saving favourite pictures like browser bookmarks.</p><p>Features:<ul><li><strong>Only links to images can be saved</strong>, so if image from link was removed, it also removes from your panel.</li><li>Images links are storing in browser. There are export and import buttons to share them between browsers.</li><li>Images are the same for site channels, but <strong>they are different for http:// and https://</strong></li></ul></p>',
+                ) +
+                '</p>',
+            ),
+          );
+
+          var $exitPicturesHelpBtn = $(
+            '<button type="button" id="help-pictures-exit-btn" class="btn btn-info" data-dismiss="modal">' +
+              app.t('favPics[.]Exit') +
+              '</button>',
+          );
+          var $footer = $('<div class="help-pictures-footer">');
+          $footer.append($exitPicturesHelpBtn);
+
+          return app.UI.createModalWindow(
+            'chat-history',
+            $header,
+            $wrapper,
+            $footer,
+          );
+        };
+        $('#help-pictures-btn').on('click', function (e) {
+          e.preventDefault();
+
+          that.showHelp();
+        });
+
+        this.exportPictures = function () {
+          var $downloadLink = $('<a>')
+            .attr({
+              href:
+                'data:text/plain;charset=utf-8,' +
+                encodeURIComponent(
+                  app.toJSON(app.storage.get('favouritePictures') || []),
+                ),
+              download: 'cytube_enhanced_favourite_images.txt',
+            })
+            .hide()
+            .appendTo($(document.body));
+
+          $downloadLink[0].click();
+
+          $downloadLink.remove();
+        };
+        $('#export-pictures').on('click', function () {
+          that.exportPictures();
+        });
+
+        this.importPictures = function (importFile) {
+          var favouritePicturesAddressesReader = new FileReader();
+
+          favouritePicturesAddressesReader.addEventListener(
+            'load',
+            function (e) {
+              var pictures = app.parseJSON(e.target.result);
+              if (_.isArray(pictures)) {
+                app.storage.set(
+                  'favouritePictures',
+                  app.parseJSON(e.target.result),
+                );
+                that.renderFavouritePictures();
+              } else {
+                app.UI.createAlertWindow(
+                  app.t("favPics[.]Can't detect any pictures in this file."),
+                );
+              }
+            },
+          );
+          favouritePicturesAddressesReader.readAsText(importFile);
+        };
+        $('#import-pictures').on('change', function () {
+          var file = $(this)[0].files[0];
+          app.UI.createConfirmWindow(
+            app.t(
+              'favPics[.]Your old pictures will be removed and replaced with the images from uploaded file (file must correspond to format of the file from export button of this panel).<br>Are you sure you want to continue?',
+            ),
+            function (isConfirmed) {
+              if (isConfirmed && file) {
+                that.importPictures(file);
+              }
+            },
+          );
+        });
+
+        // Grabbing
+        this.$favouritePicturesBodyPanel.sortable({
+          containment: this.$favouritePicturesPanelRow,
+          revert: true,
+          update: function (event, ui) {
+            var imageUrl = $(ui.item).attr('src');
+            var nextImageUrl = $(ui.item).next().attr('src');
+            var favouritePictures = app.storage.get('favouritePictures');
+
+            var imagePosition;
+            if ((imagePosition = favouritePictures.indexOf(imageUrl)) !== -1) {
+              favouritePictures.splice(imagePosition, 1);
+            } else {
+              return;
+            }
+
+            if (typeof nextImageUrl !== 'undefined') {
+              var nextImagePosition;
+              if (
+                (nextImagePosition =
+                  favouritePictures.indexOf(nextImageUrl)) !== -1
+              ) {
+                favouritePictures.splice(nextImagePosition, 0, imageUrl);
+              }
+            } else {
+              favouritePictures.push(imageUrl);
+            }
+
+            app.storage.set('favouritePictures', favouritePictures);
+          },
+        });
+
+        // Handle delete
+        this.$favouritePicturesTrash.droppable({
+          accept: '.favourite-picture-on-panel',
+          hoverClass: 'favourite-picture-drop-hover',
+          drop: function (event, ui) {
+            var imageUrl = ui.draggable.attr('src');
+            var favouritePictures = app.storage.get('favouritePictures');
+
+            var imagePosition;
+            if ((imagePosition = favouritePictures.indexOf(imageUrl)) !== -1) {
+              favouritePictures.splice(imagePosition, 1);
+              app.storage.set('favouritePictures', favouritePictures);
+            }
+
+            ui.draggable.remove();
+          },
         });
       });
 
