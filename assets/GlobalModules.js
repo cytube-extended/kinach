038ff68d16317a85cabdd474c50e0161c7d3bb9a
basevich <basevich@proton.me>
2026-04-14 04:17:14 +0400

    add and enable module 49

--- assets/GlobalModules.js
+++ assets/GlobalModules.js
@@ -72,7 +72,7 @@
       __webpack_require__(46);
       __webpack_require__(47);
       __webpack_require__(48);
-      ////__webpack_require__(49);
+      __webpack_require__(49);
       ////__webpack_require__(50);
       ////__webpack_require__(51);
       ////__webpack_require__(52);
@@ -26078,6 +26078,70 @@
 
         console.log('[cytubeEnhanced]: modPoll module is loaded!');
       });
+
+      /***/
+    },
+    /* 49 */
+    /***/ function (module, exports) {
+      window.cytubeEnhanced.addModule(
+        'videoRewindButton',
+        function (app, settings) {
+          'use strict';
+
+          var that = this;
+
+          var defaultSettings = {
+            rewindRate: 10,
+          };
+
+          settings = $.extend({}, defaultSettings, settings);
+
+          this.playerRewind = function (rewindTime) {
+            if (typeof rewindTime !== 'number') return;
+
+            PLAYER.getTime(function (c) {
+              let currentSeconds = c;
+
+              let rewindedTime = currentSeconds + rewindTime;
+
+              USEROPTS.synch = false;
+
+              PLAYER.seekTo(rewindedTime);
+
+              $('#videoRewindButton').attr('disabled', true);
+
+              let newTime = currentSeconds + Math.abs(rewindTime);
+
+              setTimeout(_ => {
+                PLAYER.seekTo(newTime);
+                USEROPTS.synch = true;
+                $('#videoRewindButton').attr('disabled', false);
+              }, Math.abs(rewindTime) * 1000);
+            });
+          };
+
+          this.setup = function () {
+            if ($('#videoRewindButton').length == 0) {
+              $('<button>')
+                .addClass('btn')
+                .addClass('btn-sm')
+                .addClass('btn-default')
+                .attr('id', 'videoRewindButton')
+                .attr('title', `Отмотать на ${settings.rewindRate} секунд`)
+                .attr('disabled', false)
+                .html(
+                  `<span class="glyphicon glyphicon-backward"><strong> ${settings.rewindRate}</strong></span>`,
+                )
+                .prependTo($('#videocontrols'))
+                .click(_ => {
+                  that.playerRewind(-settings.rewindRate);
+                });
+            }
+          };
+
+          this.setup();
+        },
+      );
 
       /***/
     },
