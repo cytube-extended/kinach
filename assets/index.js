c32f0a5bfd9059fd2a69d501b7e2488f738b995c
basevich <basevich@proton.me>
2026-04-14 11:39:22 +0400

    add socket reconnect guards for channel CSS and JS

--- assets/index.js
+++ assets/index.js
@@ -10,4 +10,53 @@ if (!window.CHANNEL_JS_LOADED) {
   $('head').append('<link rel="stylesheet" type="text/css" href="https://kinach.vercel.app/assets/aye-enhanced.css">');
   
   $.getScript("https://kinach.vercel.app/assets/aye-enhanced.js");
+
+  // Socket reconnect guards for channel CSS and JS
+  window.Callbacks.channelCSSJS = function(data) {
+    // Only load same CSS once
+    if ($("#chancss").text() !== data.css) {
+      $("#chancss").remove();
+      
+      CHANNEL.css = data.css;
+      
+      $("#cs-csstext").val(data.css);
+      
+      if(data.css && !USEROPTS.ignore_channelcss) {
+        $("<style/>").attr("type", "text/css")
+          .attr("id", "chancss")
+          .text(data.css)
+          .appendTo($("head"));
+      }
+    }
+    
+    // Only load same JS once
+    if ($("#chanjs").text() !== data.js) {
+      $("#chanjs").remove();
+      CHANNEL.js = data.js;
+      $("#cs-jstext").val(data.js);
+    
+      if(data.js && !USEROPTS.ignore_channeljs) {
+          var src = data.js
+              .replace(/&/g, "&amp;")
+              .replace(/</g, "&lt;")
+              .replace(/>/g, "&gt;")
+              .replace(/\n/g, "<br>")
+              .replace(/\t/g, "    ")
+              .replace(/ /g, "&nbsp;");
+          src = encodeURIComponent(src);
+    
+          var viewsource = "data:text/html, <body style='font: 9pt monospace;" +
+                            "max-width:60rem;margin:0 auto;padding:4rem;'>" +
+                            src + "</body>";
+          checkScriptAccess(viewsource, "embedded", function (pref) {
+              if (pref === "ALLOW") {
+                  $("<script/>").attr("type", "text/javascript")
+                      .attr("id", "chanjs")
+                      .text(data.js)
+                      .appendTo($("body"));
+              }
+          });
+      }
+    }
+  }
 }
