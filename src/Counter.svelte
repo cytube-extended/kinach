e7df75af65c297b95763bf9f44eb91aeb811ea5e
basevich <basevich@proton.me>
2026-04-16 03:44:11 +0400

    add button with tailwind style classes to counter component

--- src/Counter.svelte
+++ src/Counter.svelte
@@ -12,6 +12,9 @@
 
 <div class="svelte-counter">
   <p>Svelte counter: <strong>{count}</strong></p>
+
   <button onclick={() => count--}>-</button>
   <button onclick={() => count++}>+</button>
+
+  <button class="px-4 py-2 bg-blue-500 text-white rounded"> Click me </button>
 </div>
