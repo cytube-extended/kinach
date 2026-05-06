0d2af2957a2de2d12af1a7e8dc1d0414fd73638b
basevich <basevich@proton.me>
2026-05-06 09:42:07 +0400

    add navbar upgrade with placeholder header component

--- src/shared/utils/upgrades.ts
+++ src/shared/utils/upgrades.ts
@@ -1,12 +1,43 @@
-import { tick } from "svelte";
+import { mount, tick } from "svelte";
+import Header from "$components/layout/Header.svelte";
 
 const upgradeBody = () => {
   document.body.classList.add("dark");
   document.body.classList.add("override-theme");
 };
 
+const upgradeNavbar = async () => {
+  const navCol = document.getElementsByTagName("nav");
+  if (!navCol) {
+    throw new Error("no nav element found");
+  }
+
+  if (navCol.length < 1) {
+    throw new Error("no nav elements found");
+  }
+
+  const [navEl] = navCol;
+  if (!navEl) {
+    throw new Error("empty nav collection");
+  }
+
+  const navParent = navEl.parentElement;
+  if (!navParent) {
+    throw new Error("no nav parent found");
+  }
+
+  const toPrepend = navParent.childNodes.length > 1 && navParent.firstChild;
+
+  navEl.remove();
+  mount(Header, {
+    target: navParent,
+    anchor: toPrepend ? navParent.firstChild : undefined,
+  });
+};
+
 export const upgradeLegacyElements = async () => {
   upgradeBody();
+  await upgradeNavbar();
 
   await tick();
 };
