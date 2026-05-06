04ed9d9cdcb582b45ff8cf5d11003609ebf2ce07
basevich <basevich@proton.me>
2026-05-06 07:33:29 +0400

    add chat loading status log to entry index file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
+const notificationTimeout = 5000;
+const colorInfo =
+  "solid cyan; background-color: rgba(18, 18, 100, 0.1); color: cyan;";
+const colorError =
+  "solid pink; background-color: rgba(100, 18, 18, 0.1); color: pink;";
+
+const legacyChatNotification = (
+  message: string,
+  colorStyle: string,
+): HTMLElement | null => {
+  const msgBuf = document.getElementById("messagebuffer");
+  if (!msgBuf) {
+    return null;
+  }
+
+  const chatMessage = document.createElement("div");
+  chatMessage.className = "server-msg-reconnect";
+  chatMessage.style = `border: 1px ${colorStyle}`;
+  chatMessage.textContent = message;
+  msgBuf.appendChild(chatMessage);
+
+  return chatMessage;
+};
+
 const loadESM = () => {
+  const scriptsNotification = legacyChatNotification(
+    "Loading scripts...",
+    colorInfo,
+  );
   const url = new URL("dist/main.js", window.BASE_URL);
   const modPromise: Promise<typeof import("./main")> = import(url.toString());
 
@@ -6,11 +34,40 @@ const loadESM = () => {
     .then((mod) =>
       mod
         .init()
-        .catch((err) =>
-          console.error("failed to initialize entry module: " + err),
-        ),
+        .then(() => {
+          if (scriptsNotification) {
+            scriptsNotification.remove();
+          }
+        })
+        .catch((err) => {
+          const modInitErrMsg = `failed to initialize entry module: ${err}`;
+
+          console.error(modInitErrMsg);
+
+          const modInitNotification = legacyChatNotification(
+            modInitErrMsg,
+            colorError,
+          );
+
+          if (modInitNotification) {
+            setTimeout(modInitNotification.remove, notificationTimeout);
+          }
+        }),
     )
-    .catch((err) => console.error("failed to load entry module: " + err));
+    .catch((err) => {
+      const modLoadErrMsg = `failed to load entry module: ${err}`;
+
+      console.error(modLoadErrMsg);
+
+      const modLoadNotification = legacyChatNotification(
+        modLoadErrMsg,
+        colorError,
+      );
+
+      if (modLoadNotification) {
+        setTimeout(modLoadNotification.remove, notificationTimeout);
+      }
+    });
 };
 
 loadESM();
