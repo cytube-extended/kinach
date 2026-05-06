ee394fa60a912c4a0dee85fa4b3b067771f25685
basevich <basevich@proton.me>
2026-05-06 10:03:44 +0400

    add header body styles

--- src/shared/components/layout/Header.svelte
+++ src/shared/components/layout/Header.svelte
@@ -1,1 +1,3 @@
-<header>Header</header>
+<header
+  class="sticky w-full flex flex-row items-center justify-start h-15 mx-auto top-0 z-50 border-b bg-background md:bg-background/80 md:backdrop-blur-md"
+></header>
