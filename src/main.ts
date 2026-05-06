9cd0c140938ef746acf91b5b4ecf6efc34b2494d
basevich <basevich@proton.me>
2026-05-06 09:28:29 +0400

    remove placeholder counter component and mount script

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { initStores } from "$utils/stores";
 import { initOverrides } from "$utils/overrides";
-import { mountSvelteComponent } from "./mount";
 
 export const init = async () => {
   const unsubStores = initStores();
 
   try {
     await initOverrides();
-
-    mountSvelteComponent();
   } catch (err) {
     unsubStores();
 
