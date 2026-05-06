0fd394e74e70a10137b8d691e0ff2e9d3756253f
basevich <basevich@proton.me>
2026-05-06 06:22:59 +0400

    add dev and prod environment separation

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,32 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [svelte(), tailwindcss()],
-  build: {
-    target: "esnext",
-    outDir: "dist",
-    emptyOutDir: true,
-    cssCodeSplit: true,
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+  const outDir = isDev ? "build" : "dist";
 
-    lib: {
-      entry: ["src/index.ts", "src/main.ts", "src/index.css", "src/theme.css"],
-      formats: ["es"],
+  return {
+    plugins: [svelte(), tailwindcss()],
+    build: {
+      target: "esnext",
+      outDir,
+      emptyOutDir: true,
+      cssCodeSplit: true,
+
+      lib: {
+        entry: [
+          "src/index.ts",
+          "src/main.ts",
+          "src/index.css",
+          "src/theme.css",
+        ],
+        formats: ["es"],
+      },
     },
-  },
-  resolve: {
-    alias: {
-      $lib: path.resolve("./src/lib"),
+    resolve: {
+      alias: {
+        $lib: path.resolve("./src/lib"),
+      },
     },
-  },
+  };
 });
