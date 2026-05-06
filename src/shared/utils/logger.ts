5a10423a910f1445036c14b25540e4474d0bcc00
basevich <basevich@proton.me>
2026-05-06 09:11:48 +0400

    add logger module with legacy chat message logging

---
+++ src/shared/utils/logger.ts
@@ -0,0 +1,27 @@
+const colorInfo =
+  "solid cyan; background-color: rgba(18, 18, 100, 0.1); color: cyan;";
+const colorError =
+  "solid pink; background-color: rgba(100, 18, 18, 0.1); color: pink;";
+
+const legacyChatNotification = (
+  message: string,
+  colorStyle: string,
+): HTMLElement | null => {
+  const msgBuf = document.getElementById("messagebuffer");
+  if (!msgBuf) {
+    return null;
+  }
+
+  const chatMessage = document.createElement("div");
+  chatMessage.className = "server-msg-reconnect";
+  chatMessage.style = `border: 1px ${colorStyle}`;
+  chatMessage.textContent = message;
+  msgBuf.appendChild(chatMessage);
+
+  return chatMessage;
+};
+
+export const legacyChatInfo = (message: string) =>
+  legacyChatNotification(message, colorInfo);
+export const legacyChatError = (message: string) =>
+  legacyChatNotification(message, colorError);
