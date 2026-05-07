e7caf075d638f870930c6290d4fb1ec5bb9611de
basevich <basevich@proton.me>
2026-05-08 00:00:36 +0400

    add resizable cytube fluid layout to main

--- src/shared/components/layout/Main.svelte
+++ src/shared/components/layout/Main.svelte
@@ -1,1 +1,7 @@
-<main>main</main>
+<script lang="ts">
+  import CytubeLayout from "./CytubeFluidLayout.svelte";
+</script>
+
+<main class="flex flex-col flex-1 bg-background">
+  <CytubeLayout />
+</main>
