d630108450e2efab89909b0d7bca61cb2748092e
basevich <basevich@proton.me>
2026-05-08 02:36:36 +0400

    update cytube fluid layout add guest login form to chat section

--- src/shared/components/layout/CytubeFluidLayout.svelte
+++ src/shared/components/layout/CytubeFluidLayout.svelte
@@ -4,17 +4,26 @@
     Pane,
     PaneGroup,
   } from "$lib/components/ui/resizable/index.js";
+  import GuestLoginForm from "$features/auth/GuestLoginForm.svelte";
+  import { clientStore } from "$stores/clientStore";
 </script>
 
-<PaneGroup direction="horizontal" class="w-full flex-1 rounded-lg border-t">
+<PaneGroup direction="horizontal" class="w-full flex-1 border">
   <Pane defaultSize={45} minSize={25} maxSize={50}>
     <PaneGroup direction="vertical" class="flex-1">
       <Pane defaultSize={70}>
-        <div class="flex flex-row h-full items-center justify-center p-6">
-          <span class="font-semibold">Chat</span>
+        <div class="flex flex-col h-full">
+          <div class="flex flex-col items-center justify-center h-full">
+            Chat
+          </div>
+          {#if !$clientStore.logged_in}
+            <GuestLoginForm class="w-full" />
+          {/if}
         </div>
       </Pane>
+
       <Handle />
+
       <Pane defaultSize={30}>
         <div class="flex flex-col h-full items-center justify-center p-6">
           <span class="font-semibold">Features</span>
