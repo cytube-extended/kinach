aef30cf0668388971f5c7a73f45ee7dbed16adbe
basevich <basevich@proton.me>
2026-05-07 23:59:30 +0400

    add initial cytube fluid layout with resizable component

---
+++ src/shared/components/layout/CytubeFluidLayout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import {
+    Handle,
+    Pane,
+    PaneGroup,
+  } from "$lib/components/ui/resizable/index.js";
+</script>
+
+<PaneGroup direction="horizontal" class="w-full flex-1 rounded-lg border-t">
+  <Pane defaultSize={45} minSize={25} maxSize={50}>
+    <PaneGroup direction="vertical" class="flex-1">
+      <Pane defaultSize={70}>
+        <div class="flex flex-row h-full items-center justify-center p-6">
+          <span class="font-semibold">Chat</span>
+        </div>
+      </Pane>
+      <Handle />
+      <Pane defaultSize={30}>
+        <div class="flex flex-col h-full items-center justify-center p-6">
+          <span class="font-semibold">Features</span>
+        </div>
+      </Pane>
+    </PaneGroup>
+  </Pane>
+
+  <Handle />
+
+  <Pane defaultSize={65} minSize={50} maxSize={75}>
+    <PaneGroup direction="vertical" class="flex-1">
+      <Pane defaultSize={70}>
+        <div class="flex flex-row h-full items-center justify-center p-6">
+          <span class="font-semibold">Player</span>
+        </div>
+      </Pane>
+      <Handle />
+      <Pane defaultSize={30}>
+        <div class="flex flex-col h-full items-center justify-center p-6">
+          <span class="font-semibold">Playlist</span>
+        </div>
+      </Pane>
+    </PaneGroup>
+  </Pane>
+</PaneGroup>
--- src/shared/components/layout/Header.svelte
+++ src/shared/components/layout/Header.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <header
-  class="sticky w-full flex flex-row items-center justify-start h-15 mx-auto top-0 z-50 border-b bg-background md:bg-background/80 md:backdrop-blur-md"
+  class="sticky w-full flex flex-row items-center justify-start h-15 mx-auto top-0 z-50 bg-background md:bg-background/80 md:backdrop-blur-md"
 >
   {#if $appStore.version}
     <VersionBadge version={$appStore.version} />
