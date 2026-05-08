7b3030c020e96d01722dae5940a9a364916de201
basevich <basevich@proton.me>
2026-05-09 01:15:35 +0400

    fix userlist props type

--- src/features/userlist/Userlist.svelte
+++ src/features/userlist/Userlist.svelte
@@ -48,7 +48,7 @@
   import { userlistStore, type AddUserOutputSocketData } from "./userlist";
   import UserCard from "./UserCard.svelte";
 
-  type Props = HTMLAttributes<HTMLHtmlElement>;
+  type Props = HTMLAttributes<HTMLElement>;
 
   let userlist = $derived($userlistStore.userlist);
   let hasUsers = $derived(userlist.length > 0);
