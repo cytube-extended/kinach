1ab58abfcb8c63a783f09489fca80da24f656eb7
basevich <basevich@proton.me>
2026-05-09 18:34:40 +0400

    add initial chat feature with chatline placeholder component

---
+++ src/features/chat/ChatForm.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import type { HTMLAttributes } from "svelte/elements";
+  import { Sent02Icon } from "@hugeicons/core-free-icons";
+  import { HugeiconsIcon } from "@hugeicons/svelte";
+  import { cn } from "$lib/utils";
+  import { Button } from "$lib/components/ui/button";
+  import { Field, Group } from "$lib/components/ui/field";
+  import { Textarea } from "$lib/components/ui/textarea";
+
+  type Props = HTMLAttributes<HTMLFormElement>;
+
+  let message = $state("");
+
+  const handleSubmit = async (event: SubmitEvent) => {
+    event.preventDefault();
+
+    console.log(message);
+
+    message = "";
+  };
+
+  let { class: className }: Props = $props();
+</script>
+
+<form onsubmit={handleSubmit} class={cn(className, "flex flex-row")}>
+  <Group class="flex-1 flex flex-row">
+    <Field
+      orientation="horizontal"
+      class="flex-1 flex flex-row items-start justify-start gap-0"
+    >
+      <Textarea
+        required
+        class="flex-9 h-full rounded-none border-none resize-none"
+        autocomplete="off"
+        placeholder="Chat message"
+        bind:value={message}
+      />
+
+      <Button type="submit" class="flex-1 h-full rounded-none border-none">
+        <HugeiconsIcon
+          icon={Sent02Icon}
+          class="size-5"
+          role="button"
+          aria-label="Send"
+        />
+      </Button>
+    </Field>
+  </Group>
+</form>
--- src/shared/components/layout/CytubeFluidLayout.svelte
+++ src/shared/components/layout/CytubeFluidLayout.svelte
@@ -5,6 +5,7 @@
     PaneGroup,
   } from "$lib/components/ui/resizable/index.js";
   import Userlist from "$features/userlist/Userlist.svelte";
+  import ChatForm from "$features/chat/ChatForm.svelte";
   import GuestLoginForm from "$features/auth/GuestLoginForm.svelte";
   import { clientStore } from "$stores/clientStore";
 </script>
@@ -13,27 +14,36 @@
   <Pane defaultSize={45} minSize={25} maxSize={50}>
     <PaneGroup direction="vertical" class="flex-1">
       <Pane defaultSize={70}>
-        <div class="flex flex-col h-full">
-          <PaneGroup direction="horizontal">
-            <Pane defaultSize={20}>
-              <Userlist
-                class="flex flex-col items-start justify-start h-full px-1 py-0.5"
-              />
-            </Pane>
+        <PaneGroup direction="vertical" class="flex flex-col h-full">
+          <Pane defaultSize={90}>
+            <PaneGroup direction="horizontal">
+              <Pane defaultSize={20}>
+                <Userlist
+                  class="flex flex-col items-start justify-start h-full px-1 py-0.5"
+                />
+              </Pane>
 
-            <Handle />
+              <Handle />
 
-            <Pane
-              defaultSize={80}
-              class="flex flex-col items-center justify-center h-full"
-            >
-              Chat
+              <Pane
+                defaultSize={80}
+                class="flex flex-col items-center justify-center h-full"
+              >
+                Chat
+              </Pane>
+            </PaneGroup>
+          </Pane>
+
+          <Handle />
+
+          {#if $clientStore.logged_in}
+            <Pane defaultSize={10} minSize={10} maxSize={80} class="flex">
+              <ChatForm class="flex-1 flex-row" />
             </Pane>
-          </PaneGroup>
-          {#if !$clientStore.logged_in}
+          {:else}
             <GuestLoginForm class="w-full" />
           {/if}
-        </div>
+        </PaneGroup>
       </Pane>
 
       <Handle />
